3ebc6663dafe8d25f0505aca04653a1889d141f2
rafael.huapaya@somosoh.pe <rafael.huapaya@somosoh.pe>
2024-05-16 22:48:19 -0500

    Auto-generated commit

---
+++ chunk-NAWE3LH5.js
@@ -0,0 +1,157 @@
+import{c as w,d as S,e as E,g as s,h as V}from"./chunk-D3SVRVD6.js";import{m as y}from"./chunk-MO2HGBFH.js";import{$a as M,Fb as m,Ha as g,_ as l,ba as h,ca as p,da as u,ea as b,eb as f,gb as C,hb as _,ib as n,jb as t,kb as c,nb as x,pb as O,rb as P,yb as i,zb as v}from"./chunk-IVP2T6QE.js";function z(o,e){if(o&1){let d=x();n(0,"li",14),O("click",function(){h(d);let a=P();return p(a.closeMenu())}),n(1,"a",15)(2,"span",8),u(),n(3,"svg",9),c(4,"path",16),t()(),b(),n(5,"span",12),i(6),t()()()}if(o&2){let d=e.$implicit;g(),M("routerLink",d.path),g(5),v(d.title)}}var j=(()=>{let e=class e{constructor(){this.isOpen=!1,this.menuItems=V.map(r=>r.children??[]).flat().filter(r=>r&&r.path).filter(r=>!r.path?.includes(":")),this.menuValue=!1,this.menu_icon="bi bi-list"}openMenu(){this.menuValue=!this.menuValue,this.menu_icon=this.menuValue?"bi bi-x":"bi bi-list"}closeMenu(){this.menuValue=!1,this.menu_icon="bi bi-list"}};e.\u0275fac=function(a){return new(a||e)},e.\u0275cmp=l({type:e,selectors:[["app-menu"]],standalone:!0,features:[m],decls:23,vars:0,consts:[[1,"fixed","flex","flex-col","top-14","left-0","w-14","hover:w-64","md:w-64","bg-gray-800","h-full","text-white","transition-all","duration-300","border-none","z-10","sidebar"],[1,"overflow-y-auto","overflow-x-hidden","flex","flex-col","justify-between","flex-grow"],[1,"flex","flex-col","py-4","space-y-1"],[1,"px-5","hidden","md:block"],[1,"flex","flex-row","items-center","h-8"],[1,"text-sm","font-light","tracking-wide","text-gray-400","uppercase"],[1,"flex","flex-row","items-center","mt-5","h-8"],["href","#",1,"relative","flex","flex-row","items-center","h-10","focus:outline-none","hover:bg-blue-800","dark:hover:bg-gray-600","text-white-600","hover:text-white-800","border-l-4","border-transparent","hover:border-blue-500","dark:hover:border-gray-800","pr-6"],[1,"inline-flex","justify-center","items-center","ml-4"],["fill","none","stroke","currentColor","viewBox","0 0 24 24","xmlns","http://www.w3.org/2000/svg",1,"w-5","h-5"],["stroke-linecap","round","stroke-linejoin","round","stroke-width","2","d","M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"],["stroke-linecap","round","stroke-linejoin","round","stroke-width","2","d","M15 12a3 3 0 11-6 0 3 3 0 016 0z"],[1,"ml-2","text-sm","tracking-wide","truncate"],[1,"mb-14","px-5","py-3","hidden","md:block","text-center","text-xs"],[3,"click"],["routerLinkActive","bg-blue-800",1,"relative","flex","flex-row","items-center","h-10","focus:outline-none","hover:bg-blue-800","hover:text-white-800","border-l-4","border-transparent","hover:border-blue-500","pr-6",3,"routerLink"],["stroke-linecap","round","stroke-linejoin","round","stroke-width","2","d","M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"]],template:function(a,k){a&1&&(n(0,"div",0)(1,"div",1)(2,"ul",2)(3,"li",3)(4,"div",4)(5,"div",5),i(6,"Principal"),t()()(),C(7,z,7,2,"li",null,f),n(9,"li",3)(10,"div",6)(11,"div",5),i(12,"Otros servicios"),t()()(),n(13,"li")(14,"a",7)(15,"span",8),u(),n(16,"svg",9),c(17,"path",10)(18,"path",11),t()(),b(),n(19,"span",12),i(20,"Settings"),t()()()(),n(21,"p",13),i(22,"Copyright 2024"),t()()()),a&2&&(g(7),_(k.menuItems))},dependencies:[y,s,S,E]});let o=e;return o})();var H=(()=>{let e=class e{constructor(){this.menuValue=!1}};e.\u0275fac=function(a){return new(a||e)},e.\u0275cmp=l({type:e,selectors:[["ng-component"]],standalone:!0,features:[m],decls:22,vars:0,consts:[["x-data","setup()",1,"{","'dark':","isDark","}"],[1,"min-h-screen","flex","flex-col","flex-auto","flex-shrink-0","antialiased","text-black","bg-gray-100"],[1,"fixed","w-full","flex","items-center","justify-between","h-14","text-white","z-10"],[1,"flex","items-center","justify-start","md:justify-center","pl-3","w-14","md:w-80","h-14","bg-blue-800","dark:bg-gray-800","border-none"],["src","http://imgfz.com/i/uHs9CVP.png",1,"w-7","h-7","md:w-10","md:h-10","mr-2","rounded-md","overflow-hidden","scale-75"],[1,"hidden","md:block"],[1,"flex","justify-between","items-center","h-14","bg-blue-800","header-right"],[1,"flex","items-center","lg:w-full","max-w-xl","mr-4","p-2"],[1,"flex","items-center"],[1,"block","w-px","h-6","mx-3","bg-gray-400"],["href","#",1,"flex","items-center","mr-4","hover:text-blue-100"],["src","http://imgfz.com/i/dO2NC8r.png",1,"w-7","h-7","md:w-10","md:h-10","mr-2","rounded-md","overflow-hidden","scale-75"],[1,"font-semibold"],[1,"text-xs","text-center","text-indigo-200","font-medium"]],template:function(a,k){a&1&&(n(0,"div",0)(1,"div",1)(2,"div",2)(3,"div",3),c(4,"img",4),n(5,"span",5),i(6,"IEA Joaquin Escudo de Roble"),t()(),n(7,"div",6),c(8,"div",7),n(9,"ul",8)(10,"li"),c(11,"div",9),t(),n(12,"li")(13,"a",10),c(14,"img",11),n(15,"span")(16,"p",12),i(17,"Alberto Bravo"),t(),n(18,"p",13),i(19,"Docente"),t()()()()()()(),c(20,"app-menu")(21,"router-outlet"),t()())},dependencies:[s,w,j],styles:[`
+
+    .dark[_ngcontent-%COMP%]   .dark\\:divide-gray-700[_ngcontent-%COMP%] > [_ngcontent-%COMP%]:not([hidden]) ~ [_ngcontent-%COMP%]:not([hidden]) {
+        border-color: rgba(55, 65, 81);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-gray-50[_ngcontent-%COMP%] {
+        background-color: rgba(249, 250, 251);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-gray-100[_ngcontent-%COMP%] {
+        background-color: rgba(243, 244, 246);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-gray-600[_ngcontent-%COMP%] {
+        background-color: rgba(75, 85, 99);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-gray-700[_ngcontent-%COMP%] {
+        background-color: rgba(55, 65, 81);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-gray-800[_ngcontent-%COMP%] {
+        background-color: rgba(31, 41, 55);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-gray-900[_ngcontent-%COMP%] {
+        background-color: rgba(17, 24, 39);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-red-700[_ngcontent-%COMP%] {
+        background-color: rgba(185, 28, 28);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:bg-green-700[_ngcontent-%COMP%] {
+        background-color: rgba(4, 120, 87);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:bg-gray-200[_ngcontent-%COMP%]:hover {
+        background-color: rgba(229, 231, 235);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:bg-gray-600[_ngcontent-%COMP%]:hover {
+        background-color: rgba(75, 85, 99);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:bg-gray-700[_ngcontent-%COMP%]:hover {
+        background-color: rgba(55, 65, 81);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:bg-gray-900[_ngcontent-%COMP%]:hover {
+        background-color: rgba(17, 24, 39);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:border-gray-100[_ngcontent-%COMP%] {
+        border-color: rgba(243, 244, 246);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:border-gray-400[_ngcontent-%COMP%] {
+        border-color: rgba(156, 163, 175);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:border-gray-500[_ngcontent-%COMP%] {
+        border-color: rgba(107, 114, 128);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:border-gray-600[_ngcontent-%COMP%] {
+        border-color: rgba(75, 85, 99);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:border-gray-700[_ngcontent-%COMP%] {
+        border-color: rgba(55, 65, 81);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:border-gray-900[_ngcontent-%COMP%] {
+        border-color: rgba(17, 24, 39);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:border-gray-800[_ngcontent-%COMP%]:hover {
+        border-color: rgba(31, 41, 55);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-white[_ngcontent-%COMP%] {
+        color: rgba(255, 255, 255);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-50[_ngcontent-%COMP%] {
+        color: rgba(249, 250, 251);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-100[_ngcontent-%COMP%] {
+        color: rgba(243, 244, 246);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-200[_ngcontent-%COMP%] {
+        color: rgba(229, 231, 235);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-400[_ngcontent-%COMP%] {
+        color: rgba(156, 163, 175);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-500[_ngcontent-%COMP%] {
+        color: rgba(107, 114, 128);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-700[_ngcontent-%COMP%] {
+        color: rgba(55, 65, 81);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-gray-800[_ngcontent-%COMP%] {
+        color: rgba(31, 41, 55);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-red-100[_ngcontent-%COMP%] {
+        color: rgba(254, 226, 226);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-green-100[_ngcontent-%COMP%] {
+        color: rgba(209, 250, 229);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:text-blue-400[_ngcontent-%COMP%] {
+        color: rgba(96, 165, 250);
+    }
+
+    .dark[_ngcontent-%COMP%]   .group[_ngcontent-%COMP%]:hover   .dark\\:group-hover\\:text-gray-500[_ngcontent-%COMP%] {
+        color: rgba(107, 114, 128);
+    }
+
+    .dark[_ngcontent-%COMP%]   .group[_ngcontent-%COMP%]:focus   .dark\\:group-focus\\:text-gray-700[_ngcontent-%COMP%] {
+        color: rgba(55, 65, 81);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:text-gray-100[_ngcontent-%COMP%]:hover {
+        color: rgba(243, 244, 246);
+    }
+
+    .dark[_ngcontent-%COMP%]   .dark\\:hover\\:text-blue-500[_ngcontent-%COMP%]:hover {
+        color: rgba(59, 130, 246);
+    }
+
+    
+
+    .header-right[_ngcontent-%COMP%] {
+        width: calc(100% - 3.5rem);
+    }
+
+    .sidebar[_ngcontent-%COMP%]:hover {
+        width: 16rem;
+    }
+
+    @media only screen and (min-width: 768px) {
+        .header-right[_ngcontent-%COMP%] {
+            width: calc(100% - 16rem);
+        }
+    }`]});let o=e;return o})();export{H as default};
